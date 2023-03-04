0ec5923fdfe8cb0234d1cfc900d7ea06ffaafb22
Klaydm <klaydm@gmail.com>
2023-03-04 16:21:39 -0300

    Configuração do banco de dados

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,17 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from "next";
+import {conectarMongoDB} from "../../middlewares/conectarMongoDB";
+import type {RespostaPadraoMsg} from "../../types/RespostaPadraoMsg";
 
-export default (
+const endpointLogin = (
   req : NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<RespostaPadraoMsg>
 ) => {
-  if(req.method == "POST"){
+  if(req.method === "POST"){
     const {login, senha} = req.body;
 
     if(login === "admin@admin.com" &&
         senha === "Admin@123"){
-          res.status(200).json({msg : "Usuário autenticado com sucesso"});
+          return res.status(200).json({msg : "Usuário autenticado com sucesso"});
     }
     return res.status(400).json({erro : "Usuário ou senha não encontrados"});
   }
   return res.status(405).json({erro : "Método informado não é válido"}) ;
 }
+
+export default conectarMongoDB(endpointLogin);
