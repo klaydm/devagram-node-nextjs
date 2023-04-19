48e57f49145c65f65c032ff1fc02a3ee37d54417
Klaydm <klaydm@gmail.com>
2023-04-18 23:25:23 -0300

    Feat: Adicionada a consuta de usuarios

---
+++ src/pages/api/pesquisa.ts
@@ -0,0 +1,39 @@
+import { UsuarioModel } from './../../../models/UsuarioModel';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { conectarMongoDB } from './../../../middlewares/conectarMongoDB';
+import { validarTokenJWT } from './../../../middlewares/validarTokenJWT';
+import { RespostaPadraoMsg } from './../../../types/RespostaPadraoMsg';
+
+const pesquisaEndpoint 
+= async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg> | any[]) => {
+    try {
+      if(req.method === 'GET'){
+        if(req?.query?.id){
+          const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+          if(!usuarioEncontrado){
+            return res.status(400).json({erro : 'Usuário não encontrado'});
+          }
+          usuarioEncontrado.senha = null;
+          return res.status(200).json(usuarioEncontrado);
+        }else {
+          const {filtro} = req.query;
+          if(!filtro || filtro.length < 2){
+            return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
+          }
+          
+          const usuariosEncontrados = await UsuarioModel.find({
+            $or : [{nome : {$regex : filtro, $options : 'i'}},
+                  {email : {$regex : filtro, $options : 'i'}}]
+  
+          });
+          return res.status(200).json(usuariosEncontrados);
+        }
+        return res.status(405).json({erro : 'o método informado não é válido'});
+        }
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({erro : 'Não foi possível buscar usuários:' + e});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
--- src/pages/api/usuario.ts
+++ src/pages/api/usuario.ts
@@ -3,19 +3,58 @@ import { RespostaPadraoMsg } from './../../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../../middlewares/validarTokenJWT';
 import { conectarMongoDB } from './../../../middlewares/conectarMongoDB';
 import { UsuarioModel } from './../../../models/UsuarioModel';
+import nc from 'next-connect';
+import {upload, uploadImagemCosmic} from "../../../services/uploadImagemCosmic";
 
+const handler = nc()
+  .use(upload.single('file'))
+  .put(async(req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+      const {userId} = req?.query;
+      const usuario = await UsuarioModel.findById(userId);
+      if(!usuario){
+        return res.status(400).json({ erro : 'Usuário não encontrado'});
+      }
 
-const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg> | any) => {
+      const {nome} = req.body;
+      if(nome && nome.length > 2){
+        usuario.nome = nome;
+      }
 
-  try {
-    const {userId} = req?.query;
-    const usuario = await UsuarioModel.findById(userId);
-    usuario.senha = null;
-    return res.status(200).json(usuario);
-  } catch (e) {
-    console.log(e)
-    return res.status(400).json({erro : 'Não foi possível obter dados do usuário'});
+      const {file} = req;
+      if(file && file.originalname){
+        const image = await uploadImagemCosmic(req);
+        if(image && image.media && image.media.url){
+          usuario.avatar = image.media.url;
+        }
+      }
+
+      await UsuarioModel
+      .findByIdAndUpdate({_id : usuario._id}, usuario);
+
+      return res.status(200).json({msg : 'Usuário alterado com sucesso'});
+    } catch (e) {
+      console.log(e)
+    return res.status(400).json({ erro : 'Não foi possível atualizar o usuário.' + e})
+
+    }
+  })
+  .get(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg> | any) => {
+    try {
+      const {userId} = req?.query;
+      const usuario = await UsuarioModel.findById(userId);
+      usuario.senha = null;
+      return res.status(200).json(usuario);
+    } catch (e) {
+      console.log(e)
+      return res.status(400).json({erro : 'Não foi possível obter dados do usuário'});
+    }
+});
+
+export const config = {
+  api : {
+    bodyParser : false
   }
 }
 
-export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
+export default validarTokenJWT(conectarMongoDB(handler));
