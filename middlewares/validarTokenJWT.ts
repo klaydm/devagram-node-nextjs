40c350ca236fd6c630ba1c6978e549dd8aa90084
klaydm <klaydm@gmail.com>
2023-04-22 22:25:18 -0300

    Fix: Corrigido o decoded e adicionado ignoreBuild

--- middlewares/validarTokenJWT.ts
+++ middlewares/validarTokenJWT.ts
@@ -1,6 +1,6 @@
-import type {NextApiRequest, NextApiResponse, NextApiHandler} from "next";
-import type {RespostaPadraoMsg} from "../types/RespostaPadraoMsg";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import type { RespostaPadraoMsg } from "../types/RespostaPadraoMsg";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const validarTokenJWT = (handler : NextApiHandler) => 
   ( req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
@@ -27,7 +27,7 @@ export const validarTokenJWT = (handler : NextApiHandler) =>
         return res.status(401).json({ erro : "Não foi possível validar o token de acesso"});
       }
 
-      const decoded = jwt.verify(token, MINHA_CHAVE_JWT) as jwtPayload;
+      const decoded = jwt.verify(token, MINHA_CHAVE_JWT) as JwtPayload;
       if(!decoded){
         return res.status(401).json({ erro : "Não foi possível validar o token de acesso"});
       }
