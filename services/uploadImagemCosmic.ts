2e3cb5c67ff4fa9f44c18b460e75eafe449430fb
Kennedy Klaydm <klaydm@gmail.com>
2023-09-24 04:54:39 -0300

    Atualização das env e do código CosmicJS

--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -1,39 +1,52 @@
+import { createBucketClient } from "@cosmicjs/sdk";
 import multer from "multer";
-import cosmicjs from "cosmicjs";
 
-const {
-  CHAVE_CRIACAO_AVATARES,
-  CHAVE_CRIACAO_PUBLICACOES,
-  BUCKET_AVATARES,
-  BUCKET_PUBLICACOES} = process.env;
+const {BUCKET_SLUG, READ_KEY, WRITE_KEY} = process.env;
 
-  const Cosmic = cosmicjs();
-  const bucketAvatares = Cosmic.bucket({
-    slug: BUCKET_AVATARES,
-    write_key: CHAVE_CRIACAO_AVATARES
-  });
-
-  const bucketPublicacoes = Cosmic.bucket({
-    slug: BUCKET_PUBLICACOES,
-    write_key: CHAVE_CRIACAO_PUBLICACOES
-  });
+const bucketDevagram = createBucketClient({
+  bucketSlug: BUCKET_SLUG as string,
+  readKey: READ_KEY as string,
+  writeKey: WRITE_KEY as string,
+})
 
   const storage = multer.memoryStorage();
+
   const upload = multer({storage : storage});
 
-  const uploadImagemCosmic =async (req:any) => {
+  const uploadImagemCosmic = async (req: any) => {
     if(req?.file?.originalname){
+      if(
+        !req.file.originalname.includes(".png") &&
+        !req.file.originalname.includes(".jpg") && 
+        !req.file.originalname.includes(".jpeg")
+      ) 
+      
+      {
+        throw new Error("Extensão da imagem inválida");
+      }
+
       const media_object = {
         originalname: req.file.originalname,
         buffer : req.file.buffer
       };
 
       if(req.url && req.url.includes('publicacao')){
-        return await bucketPublicacoes.addMedia({media : media_object});
+        return await bucketDevagram.media.insertOne({
+          media : media_object,
+          folder: "publicacao",
+        });       
+      }else if(req.url && req.url.includes('avatar')){
+        return await bucketDevagram.media.insertOne({
+          media : media_object,
+          folder: "avatar",
+        })
       }else{
-        return await bucketAvatares.addMedia({media : media_object});
-      }
+        return await bucketDevagram.media.insertOne({
+          media : media_object,
+          folder: "stories",
+      });
     }
   }
+}; 
 
 export { upload, uploadImagemCosmic };
