da5f8cb91140d2481ddaa06b1f6622d3dbf751e6
Kennedy Klaydm <klaydm@gmail.com>
2023-09-24 19:16:55 -0300

    Alterada validação do JWT

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -18,14 +18,14 @@ const endpointLogin = async (
   }
 
   if(req.method === "POST"){
-    const {login, senha} = req.body;
+    const {email, senha} = req.body;
 
-    const usuariosEncontrados = await UsuarioModel.find({ email : login, senha : md5(senha)});
+    const usuariosEncontrados = await UsuarioModel.find({ email : email, senha : md5(senha)});
     if(usuariosEncontrados && usuariosEncontrados.length > 0){
       const usuarioEncontrado = usuariosEncontrados[0];
 
       const token = jwt.sign({ _id :  usuarioEncontrado._id}, MINHA_CHAVE_JWT)
-          return res.status(200).json({nome : usuarioEncontrado.nome, email : usuarioEncontrado.email, token});
+          return res.status(200).json({dados : {id : usuarioEncontrado._id, nome : usuarioEncontrado.nome, email : usuarioEncontrado.email, seguidores: usuarioEncontrado.seguidores, seguindo: usuarioEncontrado.seguindo, publicacoes: usuarioEncontrado.publicacoes, token, }});
     }
     return res.status(400).json({erro : "Usuário ou senha não encontrados"});
   }
